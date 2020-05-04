1a845169d8536046a5af3e4c998d84c41b338705
王宣棋(wangxq02) <wangxq02@zenmen.com>
2020-05-04 12:32:40 +0800

    sliver widget 学习

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 import 'package:app/demo/drawer_demo.dart';
 import 'package:app/demo/list_view_demo.dart';
 import 'package:app/demo/bottom_navigation_bar_demo.dart';
 import 'package:app/demo/basic_demo.dart';
 import 'package:app/demo/layout_demo.dart';
 import 'package:app/demo/view_demo.dart';
+import 'package:app/demo/sliver_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // 隐藏右上角 debug
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
@@ -26,7 +29,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: 5,
       child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
@@ -54,6 +57,7 @@ class Home extends StatelessWidget {
               Tab(icon: Icon(Icons.change_history)),
               Tab(icon: Icon(Icons.directions_bike)),
               Tab(icon: Icon(Icons.view_quilt)),
+              Tab(icon: Icon(Icons.access_alarm)),
             ],
           ),
         ),
@@ -63,6 +67,7 @@ class Home extends StatelessWidget {
             BasicDemo(),
             LayoutDemo(),
             ViewDemo(),
+            SliverDemo(),
             // Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
             // Icon(Icons.change_history, size: 128.0, color: Colors.black12),
             // Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -26,4 +26,49 @@ final List<Post> posts = [
     author: 'mr3',
     imgUrl: 'https://img9.doubanio.com/view/photo/albumcover/public/p2544435894.webp'
   ),
+  Post(
+    title: 'post4',
+    author: 'mr4',
+    imgUrl: 'https://img1.doubanio.com/view/photo/albumcover/public/p2534644818.webp'
+  ),
+  Post(
+    title: 'post5',
+    author: 'mr5',
+    imgUrl: 'https://img3.doubanio.com/view/photo/albumcover/public/p2496879361.webp'
+  ),
+  Post(
+    title: 'post6',
+    author: 'mr6',
+    imgUrl: 'https://img9.doubanio.com/view/photo/albumcover/public/p2544435894.webp'
+  ),
+  Post(
+    title: 'post7',
+    author: 'mr7',
+    imgUrl: 'https://img1.doubanio.com/view/photo/albumcover/public/p2534644818.webp'
+  ),
+  Post(
+    title: 'post8',
+    author: 'mr8',
+    imgUrl: 'https://img3.doubanio.com/view/photo/albumcover/public/p2496879361.webp'
+  ),
+  Post(
+    title: 'post9',
+    author: 'mr9',
+    imgUrl: 'https://img9.doubanio.com/view/photo/albumcover/public/p2544435894.webp'
+  ),
+  Post(
+    title: 'post10',
+    author: 'mr10',
+    imgUrl: 'https://img1.doubanio.com/view/photo/albumcover/public/p2534644818.webp'
+  ),
+  Post(
+    title: 'post11',
+    author: 'mr11',
+    imgUrl: 'https://img3.doubanio.com/view/photo/albumcover/public/p2496879361.webp'
+  ),
+  Post(
+    title: 'post12',
+    author: 'mr12',
+    imgUrl: 'https://img9.doubanio.com/view/photo/albumcover/public/p2544435894.webp'
+  ),
 ];
