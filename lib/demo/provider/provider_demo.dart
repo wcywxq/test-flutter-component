bff51c70577b27024d8d70b93856f4c3c9bcabad
王宣棋(wangxq02) <wangxq02@zenmen.com>
2020-05-27 09:21:14 +0800

    使用Consumer的方式使用Provider

--- lib/demo/provider/provider_demo.dart
+++ lib/demo/provider/provider_demo.dart
@@ -28,17 +28,41 @@ class CounterDemo extends StatelessWidget {
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
+
+          /// 方案一
+          // children: <Widget>[
+          //   Text(
+          //     /// 当 Counter 改变时将重建小部件
+          //     '${context.watch<Counter>().count}',
+          //     // '${Provider.of<Counter>(context).count}',
+          //     style: Theme.of(context).textTheme.headline4,
+          //   ),
+          //   CupertinoButton(
+          //     child: Icon(CupertinoIcons.minus_circled, color: CupertinoColors.white),
+          //     onPressed: () => context.read<Counter>().decrement(),
+          //     color: CupertinoColors.activeBlue,
+          //   ),
+          // ],
+          /// 方案二
           children: <Widget>[
-            Text(
-              /// 当 Counter 改变时将重建小部件
-              '${context.watch<Counter>().count}',
-              // '${Provider.of<Counter>(context).count}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            CupertinoButton(
-              child: Icon(CupertinoIcons.minus_circled, color: CupertinoColors.white),
-              onPressed: () => context.read<Counter>().decrement(),
-              color: CupertinoColors.activeBlue,
+            Consumer<Counter>(
+              builder: (BuildContext context, Counter counter, Widget child) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '${counter.count}',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    CupertinoButton(
+                      child: Icon(CupertinoIcons.minus_circled,
+                          color: CupertinoColors.white),
+                      onPressed: () => counter.decrement(),
+                      color: CupertinoColors.activeBlue,
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),
